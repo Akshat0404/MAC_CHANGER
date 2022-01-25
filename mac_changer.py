812b4f5ea56eb16f83028ec2b8f9a5b73aba4452
Akshat0404 <91226038+Akshat0404@users.noreply.github.com>
2022-01-25 22:43:46 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,25 +1,26 @@
 import subprocess
 import optparse
 import re
+from getmac import get_mac_address
 
 
 def argument():
-    parser = optparse.OptionParser()
-    parser.add_option("-n", "--network_interface", dest="network_interface", help="Name of the network interface of "
+    Parser = optparse.OptionParser()
+    Parser.add_option("-n", "--network_interface", dest="network_interface", help="Name of the network interface of "
                                                                                   "which the MAC address has "
                                                                                   "to be changed")
-    parser.add_option("-c", "--new_mac", dest="new_mac", help="New MAC address")
-    (option, arguments) = parser.parse_args()
+    Parser.add_option("-c", "--new_mac", dest="new_mac", help="New MAC address")
+    (option, arguments) = Parser.parse_args()
     if not option.network_interface:
-        parser.error("[-] Please specify a network interface. Use --help or -h for more info.")
+        Parser.error("[-] Please specify a network interface. Use --help or -h for more info.")
     if not option.new_mac:
-        parser.error("[-] Please enter the new MAC address. Use --help or -h for more info.")
+        Parser.error("[-] Please enter the new MAC address. Use --help or -h for more info.")
     while 1:
         if re.match(r"^[a-fA-F0-9]{2}[:][a-fA-F0-9]{2}[:][a-fA-F0-9]{2}[:][a-fA-F0-9]{2}[:][a-fA-F0-9]{2}[:]["
                     r"a-fA-F0-9]{2}", option.new_mac):
             break
         else:
-            parser.error("[-] You have specified an incorrect format for MAC address. Please Enter the MAC address in "
+            Parser.error("[-] You have specified an incorrect format for MAC address. Please Enter the MAC address in "
                          "the "
                          "format "
                          "xx:xx:xx:xx:xx:xx, where x is lower case alphabets from a-f or upper case alphabets from "
@@ -32,10 +33,10 @@ def argument():
         if option.network_interface == "wlan0":
             break
         if option.network_interface == "lo":
-            parser.error("[-] This network interface does not have MAC address")
+            Parser.error("[-] This network interface does not have a MAC address")
             break
         else:
-            parser.error("[-] " + option.network_interface + " is not a valid network interface. Please specify a"
+            Parser.error("[-] " + option.network_interface + " is not a valid network interface. Please specify a"
                                                              " valid network interface.")
             break
     return option
@@ -48,5 +49,21 @@ def mac(network_interface, new_mac):
     subprocess.call(["ifconfig", network_interface, "up"])
 
 
+def check(interface):
+    check_mac_address = get_mac_address(interface)
+    return check_mac_address
+
+
 options = argument()
+check_mac = check(options.network_interface)
+print("[+] Current MAC is ", check_mac)
 mac(options.network_interface, options.new_mac)
+check_mac = check(options.network_interface)
+while 1:
+    parser = optparse.OptionParser()
+    if check_mac == options.new_mac:
+        print("[+] The new MAC address is ", check_mac)
+        break
+    else:
+        parser.error("[-] Failed to change the MAC address")
+        break
